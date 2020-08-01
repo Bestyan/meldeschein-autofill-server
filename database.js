344668d8911885ec4dce186e527d949ad6e3f3ce
Bastian Schattenberg <git@bestyan.de>
2020-08-01 11:04:15 +0200

    chore: remove /get-firstnames-count

--- database.js
+++ database.js
@@ -100,20 +100,6 @@ module.exports = {
         })
     },
 
-    getFirstnameCount() {
-        return new Promise((resolve, reject) => {
-            this.firstnames.count({}, (error, number) => {
-
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                }
-
-                resolve({firstnames: number});
-            })
-        });
-    },
-
     /**
      * 
      * @param {*} firstname 
--- index.js
+++ index.js
@@ -103,14 +103,6 @@ app.get('/db/get-firstname', (request, response) => {
 
 });
 
-app.get('/db/get-firstname-count', (request, response) => {
-
-    db.getFirstnameCount()
-        .then(number => response.json(constants.getDataResponse(number)))
-        .catch(error => response.json(constants.getErrorResponse(error)));
-
-});
-
 /**
  * expects the body to look like this:
  *   {
